3f4126511ffd5996d1f844ab666975ca7c7c9ead
00197mfz <golammafuzgm@gmail.com>
2026-01-10 19:54:31 +0530

    refactor(dim): align line dimension with snapshot-based SCD Type 2 logic

--- dbt/models/dimensions/dim_lines.sql
+++ dbt/models/dimensions/dim_lines.sql
@@ -1,37 +1,23 @@
 /*
     Dimension: Lines (SCD Type 2)
-    
-    Tracks changes to transit line attributes over time.
 */
 
-with seed_lines as (
-    select * from {{ ref('seed_lines') }}
-),
-
-current_lines as (
-    select
-        line_id,
-        line_name,
-        line_type,
-        operator_name,
-        -- SCD Type 2 fields
-        current_date as valid_from,
-        cast('{{ var("scd_end_date") }}' as date) as valid_to,
-        true as is_current
-    from seed_lines
+with snapshot_source as (
+    -- Logic depends on creating dbt/snapshots/dim_lines_snapshot.sql
+    select * from {{ ref('dim_lines_snapshot') }}
 ),
 
 final as (
     select
-        {{ dbt_utils.generate_surrogate_key(['line_id', 'valid_from']) }} as line_key,
+        {{ dbt_utils.generate_surrogate_key(['line_id', 'dbt_valid_from']) }} as line_key,
         line_id,
         line_name,
         line_type,
         operator_name,
-        valid_from,
-        valid_to,
-        is_current
-    from current_lines
+        dbt_valid_from as valid_from,
+        coalesce(dbt_valid_to, cast('{{ var("scd_end_date") }}' as date)) as valid_to,
+        (dbt_valid_to is null) as is_current
+    from snapshot_source
 )
 
 select * from final
