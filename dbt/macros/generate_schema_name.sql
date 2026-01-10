ba0ca9a1a7adc88e632651bef492dcb442b78267
00197mfz <golammafuzgm@gmail.com>
2026-01-10 19:52:12 +0530

    refactor(dbt): standardize schema name generation for clean environment isolation

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,10 +1,3 @@
-/*
-    Override default schema name generation.
-    
-    By default, dbt creates schemas like: target_schema_custom_schema
-    This macro keeps just the custom schema name.
-*/
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
